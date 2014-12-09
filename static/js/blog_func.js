5946145be3c83bd828893dc82d58cd418dcc029b
Darshith <darshithb@mindfiresolutions.com>
2014-12-09 18:06:57 +0530

    changes for making the text bold.

--- static/js/blog_func.js
+++ static/js/blog_func.js
@@ -1,8 +1,56 @@
-$('.embolden').click(function(){
-    var highlight = window.getSelection();  
-    var span = '<span class="bold">' + highlight + '</span>';
-    var text = $('.embolden').html();
-    $('.embolden').html(text.replace(highlight, span));
+// $(document).ready(function(){
+
+// 	$(document).on('click', '#buttonB', function(){
+// 		// alert("button clicked");
+// 		alert(window.getSelection);
+//     	// var highlight = window.getSelection(); 
+//     	if (window.getSelection){
+//     		try {
+//     			var ta = $('#body-blog').get(0);
+//             	var newText = ta.value.substring(ta.selectionStart, ta.selectionEnd);
+//             	alert(newText);
+//             	var selectedText = newText.toString();
+//     			var span = '<span class="bold">' + newText + '</span>';
+//     			alert(span);
+//     			var text = $('.bold').html();
+//     			$('#body-blog').html(text.replace(newText, span));
+//     		}
+//     		catch (e){
+//     			alert("cant get selected text");
+//     		}
+//     	} 
+//     });
+
+// });
+
+$(document).ready(function(){
+	$(document).on('click', '#buttonB', function(){
+		ShowSelection();
+	});
+
 });
 
-document.execCommand('bold');
+
+function ShowSelection()
+{
+  var textComponent = document.getElementById('body-blog');
+  var selectedText;
+  // IE version
+  if (document.selection != undefined)
+  {
+    textComponent.focus();
+    var sel = document.selection.createRange();
+    selectedText = sel.text;
+  }
+  // Mozilla version
+  else if (textComponent.selectionStart != undefined)
+  {
+    var startPos = textComponent.selectionStart;
+    var endPos = textComponent.selectionEnd;
+    selectedText = textComponent.value.substring(startPos, endPos);
+  }
+  alert("You selected: " + selectedText);
+}
+
+
+// document.execCommand('bold');
