731949038c46e12f7c846c6edba7bb57b884c984
Darshith <darshithb@mindfiresolutions.com>
2014-12-11 10:33:00 +0530

    changes for adding CKEditor.

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from myapp.models import Blog, Category
+from myapp.models import Blog, Category, Entry
 from django.conf import settings
+from django.db import models
+from django import forms
 
 
 class BlogAdmin(admin.ModelAdmin):
     exclude = ['posted']
     prepopulated_fields = {'slug': ('title',)}
+
+ #    formfield_overrides = {models.TextField: {'widget': forms.Textarea(attrs={'class': 'ckeditor'})},}
+	# list_display= ['title']
+
+	# class Media():
+	# 	js = ('ckeditor/ckeditor.js',)
+
 
     def __init__(self, model, admin_site):
         super(BlogAdmin, self).__init__(model, admin_site)
@@ -17,5 +26,17 @@ class CategoryAdmin(admin.ModelAdmin):
     def __init__(self, model, admin_site):
         super(CategoryAdmin, self).__init__(model, admin_site)
 
+
+class EntryAdmin(admin.ModelAdmin):
+	"""docstring for ClassName"""
+	formfield_overrides = {models.TextField: {'widget': forms.Textarea(attrs={'class': 'ckeditor'})},}
+	list_display= ['title']
+
+	class Media():
+		js = ('ckeditor/ckeditor.js',)
+
+
+
+admin.site.register(Entry, EntryAdmin)
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(Category, CategoryAdmin)
